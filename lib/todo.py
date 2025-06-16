7565a28d37781c2c91e62fbb6f0859e199bc1a3f
Zuhair <khanzuhair96@live.co.uk>
2025-06-16 11:39:00 +0100

    Solutions for toDo

--- lib/todo.py
+++ lib/todo.py
@@ -12,22 +12,21 @@ class toDo:
      
 # As a user So that I can keep track of my tasks I want a program that I can add todo tasks to and see a list of them.
      def __init__(self):
-          self.tasks = [] # Initialize a list to keep track of tasks
+        #   self.tasks = [] # Initialize a list to keep track of tasks
+        self.tasks = [] # Now storing tasks as dicts with 'description' and 'completed' keys
 
      def add_task(self, task):
-         self.tasks.append(task) # Add the given task to the list
+         self.tasks.append({"Description": task, "Completed": False}) # Add a task as a dict with completed False initially --> Key: "Description" / Value: Task | Key: "Completed" / Value: False
+
      
      def get_tasks(self):
-          return self.tasks # Return the current list of tasks
-      
-# As a user So that I can focus on tasks to complete I want to mark tasks as complete and have them disappear from the list.
-# 1. Change the task representation from a string to a dict or a small object with:
-#    - task description
-#    - completed status (True/False)
+        #   return self.tasks # Return the current list of tasks
+        return [task["Description"] for task in self.tasks if not task["Completed"]] # Return a list of descriptions for all tasks that are not completed
 
-# 2. Modify add_task to store this richer task object
 
-# 3. Create a method, e.g., mark_complete(task_description)
-#    - It should find the task and mark it completed
-
-# 4. Update get_tasks to return only tasks that are NOT completed
+     
+     def mark_complete(self, task):
+        for task_item in self.tasks:
+            if task_item["Description"] == task:
+                task_item["Completed"] = True
+                break
